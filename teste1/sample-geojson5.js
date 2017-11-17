b46c2dc59d8eb3b8aded49da248be0f95e061dc9
Elton Santos <eltonaxl@hotmail.com>
2017-11-17 16:57:00 -0300

    Initial commit

---
+++ teste1/sample-geojson5.js
@@ -0,0 +1,10 @@
+var coorsField = {
+    "type": "Feature",
+    "properties": {
+        "popupContent": "Coors Field"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404191970824, 39.756213909328125]
+    }
+};
